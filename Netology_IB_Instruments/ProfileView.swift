745e2ae79924b77006cb1f9ffd2df2af855f2fb3
deshabml <deshab@yandex.ru>
2022-11-18 17:53:41 +0300

    Create Profile storyboard & VievController

--- Netology_IB_Instruments/ProfileView.swift
+++ Netology_IB_Instruments/ProfileView.swift
@@ -9,6 +9,24 @@ import UIKit
 
 class ProfileView: UIView {
 
+    @IBOutlet weak var img: UIImageView!
+
+    @IBOutlet weak var name: UILabel!
+
+    @IBOutlet weak var dateOfBirth: UILabel!
+    
+    @IBOutlet weak var city: UILabel!
+    
+    @IBOutlet weak var signature: UITextView!
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        name.text = "Имя"
+        dateOfBirth.text = "Дата Рождения"
+        city.text = "Город"
+    }
+
+
     /*
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
